54a23d895b8af47d2623d5265eeb36c66374ee5b
Matheus Mattos <matheusbmtt@gmail.com>
2024-12-14 00:59:52 -0300

    Update Mago.java

--- HistoriaJogo/Mago.java
+++ HistoriaJogo/Mago.java
@@ -1,15 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package HistoriaJogo;
 
 import java.util.Scanner;
 
-/**
- *
- * @author MATHEUSBRAGAMATTOS
- */
+
 public class Mago implements Personagem {
     private String nome;
     private int forca;
