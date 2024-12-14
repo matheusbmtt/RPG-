aca9219253a67d14d69dbca80c0f45b666057a57
Matheus Mattos <matheusbmtt@gmail.com>
2024-12-14 00:59:43 -0300

    Update JogoAventura.java

--- HistoriaJogo/JogoAventura.java
+++ HistoriaJogo/JogoAventura.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package HistoriaJogo;
 
-/**
- *
- * @author MATHEUSBRAGAMATTOS
- */
 import java.util.Random;
 import java.util.Scanner;
 
