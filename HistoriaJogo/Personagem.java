c56491fdc2990863280cedae8bd1ef761227c650
Matheus Mattos <matheusbmtt@gmail.com>
2024-12-14 01:00:07 -0300

    Update Personagem.java

--- HistoriaJogo/Personagem.java
+++ HistoriaJogo/Personagem.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package HistoriaJogo;
 
-/**
- *
- * @author MATHEUSBRAGAMATTOS
- */
-// Personagem.java (Interface)
-// Personagem.java (Interface)
+
 public interface Personagem {
     void sortearForca();
     void sortearVida();
